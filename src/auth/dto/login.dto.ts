3aa0d126a43d182e979342a51fbaddd8e317cd21
Fernando Otáñez <omfer3.0@gmail.com>
2025-12-03 12:09:54 -0600

    <feature>(auth) Agregando funcionalidades para LogIn

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,13 @@
-import { Transform } from 'class-transformer';
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { Transform } from "class-transformer";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class LoginDto {
-  @IsEmail()
-  email: string;
 
-  @Transform(({ value }) => value.trim())
-  @IsString()
-  @MinLength(6)
-  password: string;
+    @IsEmail()
+    email: string;
+    
+    @IsString()
+    @MinLength(6)
+    @Transform(({ value }) => value.trim())
+    password: string;
 }
